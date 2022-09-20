1d5d7cb428b43d0f65daebb114fad5b78fa67eda
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2022-09-20 09:14:36 -0600

    9.20 9:14am

--- script.js
+++ script.js
@@ -1,5 +1,72 @@
-function onButtonPress() {
-    let data = prompt("enter your data")
-    alert("data")
-    console.log(data)
+//FUNCTIONS ILL NEED
+//addList
+//removeList
+//removeTodo
+//markTodoAsCompleted
+//removeAllTodosCompleted
+
+
+
+
+
+const lists = {
+    1: {title:"grocery list", tasks:[{name: 'apples', completed:false}, {name: 'bananas', completed:false}] },
+
+    2: {
+        title:"grocery list two",
+        tasks:[
+        {name: 'milk', completed:false},
+        {name: 'cheese', completed:false},
+    ]
 }
+}
+
+const currentList = lists[0];
+
+
+function render() {
+    // this will hold the html that will be displayed in the sidebar
+    let listsHtml = '<ul class="list-group">';
+    // iterate through the lists to get their names
+    lists.forEach((list) => {
+      listsHtml += `<li class="list-group-item">${list.name}</li>`;
+    });
+   
+    listsHtml += '</ul>';
+    // print out the lists
+   
+    document.getElementById('lists').innerHTML = listsHtml;
+    // print out the name of the current list
+   
+    document.getElementById('current-list-name').innerText = currentList.name;
+    // iterate over the todos in the current list
+   
+    let todosHtml = '<ul class="list-group-flush">';
+    currentList.todos.forEach((list) => {
+      todosHtml += `<li class="list-group-item">${todo.text}</li>`;
+    });
+    // print out the todos
+    document.getElementById('current-list-todos').innerHTML = todosHtml;
+   }
+
+
+   function addTodo() {
+    // get the todo text from the todo input box
+    const text = document.getElementById('todo-input-box').value;
+    if(text) {
+      currentList.todos.push({
+        text: text,
+        completed: false
+      })
+      render();
+    }
+   }
+
+
+   function save() {
+    localStorage.setItem('currentList', JSON.stringify(currentList)); 
+    localStorage.setItem('lists', JSON.stringify(lists));
+   }
+
+
+
