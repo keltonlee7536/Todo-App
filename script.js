7095d385e40b7600ff6e8522ed509d9b78d22ba2
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2022-09-22 10:20:43 -0600

    9.22 10:20am

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-//FUNCTIONS ILL NEED
-//addList
-//removeList
-//removeTodo
-//markTodoAsCompleted
-//removeAllTodosCompleted
-
- function render() {
+function removeList(){
+    //delete the list by position?
+}
+const lists = [ //add a tutorial?
+    {name:"grocery list", tasks:[{name: 'apples', completed:false}, {name: 'bananas', completed:false}]},
+    {name:"grocery list two", tasks:[ {name: 'milk', completed:false}, {name: 'cheese', completed:false}]}
+]
+function removeTask(){ //sudo
+    //if(delete)true;
+    //remove from list;
+        //render()
+}
+function completeTask(){ //sudo
+    //if (completed): true;
+        //strikethrough, move to bottom if list;
+        //render()
+}
+function removeAllTasks(){ //sudo
+    //change completed to true for currentlist
+}
+ function render() { //not sure what's going on here
     // this will hold the html that will be displayed in the sidebar
      let listsHtml = '<ul class="list-group">';
      // iterate through the lists to get their names
@@ -15,42 +28,43 @@
    
     listsHtml += '</ul>';
     // print out the lists
-   
-    document.getElementById('lists').innerHTML = listsHtml;
+    document.getElementById('displayLists').innerHTML = lists;
     //print out the name of the current list
    
-    document.getElementById('current-list-name').innerText = currentList.name;
+    document.getElementById('listname').innerText = currentList.name;
     // iterate over the todos in the current list
-   
     let todosHtml = '<ul class="list-group-flush">';
     currentList.todos.forEach((list) => {
     todosHtml += `<li class="list-group-item">${todo.text}</li>`;
     });
-//     // print out the todos
+    // print out the todos
     document.getElementById('current-list-todos').innerHTML = todosHtml;
-    }
-
-
+}
 function addTask() {
-//     // get the todo text from the todo input box
-    const text = document.getElementById('addTask').value;
-    if(text) {
-    //currentList.todo
-    lists.tasks.push({              //what? how can i reference the tasks of the current list?
-    text: text,
-    completed: false
-    })
-    render();
+        const newTask = document.getElementById('addTask').value;
+        console.log(newTask);
+        if(newTask) {
+        lists.push({
+        name: newTask,
+        completed: false
+        })
+        document.getElementById('displayList').innerHTML = lists;
+        //render();
+        }
+}
+function addList() {
+    const newList = document.getElementById('addList').value;
+    console.log(newList);
+    if(newList) {
+    lists.push
+    document.getElementById('listName').innerHTML = lists.name;
+    //render();
     }
-    }
-
-
+}
 function save() {
 localStorage.setItem('currentList', JSON.stringify(currentList)); 
 localStorage.setItem('lists', JSON.stringify(lists));
 }
-
-
 function getTasksHtml(tasks){
     let tasksHtml = "";
     for (let taskItem of tasks) {
@@ -61,25 +75,16 @@ function getTasksHtml(tasks){
     }
     return tasksHtml
 }
-
 function displayList(listData){
     let name = listData.name;
     let tasks = listData.tasks;
     let tasksHtml = getTasksHtml(tasks);
     let listName = document.querySelector('.listName')
-    
     let tasksElement = document.querySelector(".tasks")
-
 
     listName.textContent = name
     tasksElement.innerHTML = tasksHtml
 }
-
-const lists = [
-{name:"grocery list", tasks:[{name: 'apples', completed:false}, {name: 'bananas', completed:false}]},
-{name:"grocery list two", tasks:[ {name: 'milk', completed:false}, {name: 'cheese', completed:false}]}
-]
-
 function changeList(position) {
     if(position >= lists.length) {
         alert('No list exists')
@@ -87,5 +92,3 @@ function changeList(position) {
         displayList(lists[position])
     }
 }
-
-
