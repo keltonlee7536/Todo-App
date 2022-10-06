8359fc458bf4e3d0dd68acf083ed3149ef48e1c3
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2022-10-06 08:59:43 -0600

    10.6 8:59am

--- script.js
+++ script.js
@@ -1,94 +1,20 @@
-function removeList(){
-    //delete the list by position?
+function getAvailableCourses() {
+    return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
+      function (response) {
+        return response.json();
+      }
+    );
 }
-const lists = [ //add a tutorial?
-    {name:"grocery list", tasks:[{name: 'apples', completed:false}, {name: 'bananas', completed:false}]},
-    {name:"grocery list two", tasks:[ {name: 'milk', completed:false}, {name: 'cheese', completed:false}]}
-]
-function removeTask(){ //sudo
-    //if(delete)true;
-    //remove from list;
-        //render()
-}
-function completeTask(){ //sudo
-    //if (completed): true;
-        //strikethrough, move to bottom if list;
-        //render()
-}
-function removeAllTasks(){ //sudo
-    //change completed to true for currentlist
-}
- function render() { //not sure what's going on here
-    // this will hold the html that will be displayed in the sidebar
-     let listsHtml = '<ul class="list-group">';
-     // iterate through the lists to get their names
-     lists.forEach((list) => {
-    listsHtml += `<li class="list-group-item">${list.name}</li>`;
-    });
    
-    listsHtml += '</ul>';
-    // print out the lists
-    document.getElementById('displayLists').innerHTML = lists;
-    //print out the name of the current list
-   
-    document.getElementById('listname').innerText = currentList.name;
-    // iterate over the todos in the current list
-    let todosHtml = '<ul class="list-group-flush">';
-    currentList.todos.forEach((list) => {
-    todosHtml += `<li class="list-group-item">${todo.text}</li>`;
-    });
-    // print out the todos
-    document.getElementById('current-list-todos').innerHTML = todosHtml;
+const lists = {
+1: {name: 'Shopping list'},
+2: {name: 'Honey do list'},
+...
 }
-function addTask() {
-        const newTask = document.getElementById('addTask').value;
-        console.log(newTask);
-        if(newTask) {
-        lists.push({
-        name: newTask,
-        completed: false
-        })
-        document.getElementById('displayList').innerHTML = lists;
-        //render();
-        }
-}
-function addList() {
-    const newList = document.getElementById('addList').value;
-    console.log(newList);
-    if(newList) {
-    lists.push
-    document.getElementById('listName').innerHTML = lists.name;
-    //render();
-    }
-}
-function save() {
-localStorage.setItem('currentList', JSON.stringify(currentList)); 
-localStorage.setItem('lists', JSON.stringify(lists));
-}
-function getTasksHtml(tasks){
-    let tasksHtml = "";
-    for (let taskItem of tasks) {
-        tasksHtml += `
-            <input type="checkbox" ${taskItem.completed ? 'checked' : ''}" />
-            <label>${taskItem.name}</label>
-        `
-    }
-    return tasksHtml
-}
-function displayList(listData){
-    let name = listData.name;
-    let tasks = listData.tasks;
-    let tasksHtml = getTasksHtml(tasks);
-    let listName = document.querySelector('.listName')
-    let tasksElement = document.querySelector(".tasks")
 
-    listName.textContent = name
-    tasksElement.innerHTML = tasksHtml
-}
-function changeList(position) {
-    if(position >= lists.length) {
-        alert('No list exists')
-    } else {
-        displayList(lists[position])
-    }
-}
+let courseOptionsHtml = '';
+courses.forEach((course) => {
+ courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+});
+
+document.getElementById('course-select').innerHTML = courseOptionsHtml;
